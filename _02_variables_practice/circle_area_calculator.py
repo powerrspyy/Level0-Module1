84d50237e40c9a9dcfb9d3811a2c859019c5b7ef
league <powerrspyy@gmail.com>
2024-01-19 16:55:57 -0800

    started unbirthday.py

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -1,6 +1,6 @@
 import turtle
 from tkinter import messagebox, simpledialog, Tk
-import math
+import math, time
 
 # Goal: Write a Python program that asks the user for the radius 
 #       of a circle and displays the area of that circle.
@@ -13,22 +13,26 @@ if __name__ == '__main__':
     
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
-    
+    def intgr(title, prompt):
+        integer = simpledialog.askinteger(title, prompt)
+        return int(integer)
     # Make a new turtle
-    
+    radius = intgr("Radius", "Enter the radius of a circle")
+    turtle.circle(radius=radius)
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
-
+    turtle.penup()
     # Call the turtle .penup() method
-
+    turtle.goto(20,20)
     # Move your turtle to a new x,y position using .goto()
+    area = math.pi * (radius ** 2)
 
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    turtle.write(arg="area = "+ str(area), move = True, align = 'left', font = ('Arial', 30,'normal'))
     # Write the area of your circle using the turtle .write() method
     # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
-
     # Hide your turtle
-
+    turtle.hideturtle()
     # Call turtle.done()
+    turtle.done()
---
+++ _03_if_else/_1_unbirthday/unbirthday.py
@@ -0,0 +1,5 @@
+from tkinter import messagebox,simpledialog, Tk
+
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw
